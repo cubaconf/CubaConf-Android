e334164205338f8023d963092ffa743555b04e19
Jorge Alberto Díaz Orozco <diazorozcoj@gmail.com>
2017-08-21 17:58:34 -0400

    - no menu

--- app/src/main/java/cu/cubaconf/Main.java
+++ app/src/main/java/cu/cubaconf/Main.java
@@ -9,8 +9,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -32,28 +30,6 @@ public class Main extends AppCompatActivity {
         textViewTitle.setTypeface(type);
         preferences = getSharedPreferences("CubaConf", MODE_PRIVATE);
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
 
     public void openURI(String uri) {
         Intent callIntent = new Intent(Intent.ACTION_VIEW);
