65c709a61e77d3ac7edf0159a9146ecad783e562
Jorge Alberto Díaz Orozco <diazorozcoj@gmail.com>
2017-08-21 21:43:35 -0400

    - Search submissions  - Randomize submissions list  - hide keyboard on enter

--- app/src/main/java/cu/cubaconf/Submissions.java
+++ app/src/main/java/cu/cubaconf/Submissions.java
@@ -3,7 +3,11 @@ package cu.cubaconf;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.MenuItem;
+import android.view.WindowManager;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import com.google.gson.Gson;
@@ -11,11 +15,19 @@ import com.google.gson.Gson;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 import cu.cubaconf.adapter.SubmissionAdapter;
 import cu.cubaconf.model.Submission;
 
 public class Submissions extends AppCompatActivity {
+
+    public EditText search;
+    public ListView submissionsListView;
+    private Reader reader;
+    private Submission[] submissions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,20 +35,62 @@ public class Submissions extends AppCompatActivity {
         setContentView(R.layout.activity_submissions);
 
         ActionBar bar = getSupportActionBar();
-        bar.setTitle("");
+        bar.setTitle(getString(R.string.proposals));
         bar.setDisplayOptions(ActionBar.DISPLAY_HOME_AS_UP | ActionBar.DISPLAY_SHOW_CUSTOM | ActionBar.DISPLAY_SHOW_TITLE);
         bar.setHomeButtonEnabled(true);
 
+        submissionsListView = (ListView) findViewById(R.id.submissionsList);
+
         Gson GSON = new Gson();
 
-        ListView submissionsListView = (ListView) findViewById(R.id.submissionsList);
         try {
-            Reader reader = new InputStreamReader(getAssets().open("data/submissions.json"));
-            Submission[] submissions = GSON.fromJson(reader, Submission[].class);
-            submissionsListView.setAdapter(new SubmissionAdapter(this, submissions));
+            reader = new InputStreamReader(getAssets().open("data/submissions.json"));
+            submissions = GSON.fromJson(reader, Submission[].class);
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+
+        search = (EditText) findViewById(R.id.EditTextSearch);
+
+        search.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                ArrayList<Submission> newsubs = new ArrayList<Submission>();
+
+                for (Submission submission : submissions) {
+                    if (submission.getSummary().toLowerCase().contains(String.valueOf(s).toLowerCase())
+                            || submission.getName().toLowerCase().contains(String.valueOf(s).toLowerCase())
+                            || submission.getTitle().toLowerCase().contains(String.valueOf(s).toLowerCase())) {
+                        newsubs.add(submission);
+                    }
+                }
+
+                submissionsListView.setAdapter(new SubmissionAdapter(getBaseContext(), newsubs.toArray(new Submission[newsubs.size()])));
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        Collections.shuffle(Arrays.asList(submissions));
+
+        submissionsListView.setAdapter(new SubmissionAdapter(this, submissions));
+
+        hideKeyboard();
+    }
+
+    public void hideKeyboard() {
+        getWindow().setSoftInputMode(
+                WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN
+        );
     }
 
     @Override
